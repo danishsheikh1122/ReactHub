4d4bd2adaf730b24784ac104df33b40b4a6e9138
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2024-06-05 19:31:50 +0530

    second commit form danish

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
+import Section4 from "./components/Section4";
 
 // font-bricolage theme font 
 // #CFCFCF theme color
@@ -14,6 +15,7 @@ const App = () => {
       <Hero></Hero>
       <Section2></Section2>
       <Section3></Section3>
+      <Section4></Section4>
       
     </>
   );
