489a6027b21ab6bf22c0381c5f46824a73e2ab96
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2024-06-06 13:31:55 +0530

    4 commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,15 @@ const NavBar = () => {
   return (
 <div className="navbar  border-b-[1px] border-[#E6AC18] text-[#CFCFCF] bg-[#212121] font-bricolage px-[10rem]">
   <div className="flex-1">
+
     <a className="btn btn-ghost text-[2rem] hover:bg-[#E6AC18] ">ReactHub</a>
   </div>
   <div className=" w-full px-12 flex justify-start items-center ">
     <ul className="menu menu-horizontal px-1 gap-[0.8rem] text-[1.2rem]">
-      <li><a className="hover:bg-[#E6AC18] ">Link</a></li>
-      <li><a className="hover:bg-[#E6AC18] ">Link</a></li>
-      <li><a className="hover:bg-[#E6AC18] ">Link</a></li>
-      <li><a className="hover:bg-[#E6AC18] ">Link</a></li>
+      <li><a className="hover:bg-[#E6AC18] ">Pricing</a></li>
+      <li><a className="hover:bg-[#E6AC18] ">Demo</a></li>
+      <li><a className="hover:bg-[#E6AC18] ">Wall of love</a></li>
+      <li><a className="hover:bg-[#E6AC18] ">Leaderboard</a></li>
      
     </ul>
   </div>
